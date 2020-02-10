39e98a324100a824ef4808dd4480e4e2974719ea
Ashish Jha <ashish.jha@revolut.com>
2020-02-10 12:48:44 +0000

    corrected unittests mistake in chapter 5 and edited notebook titles in line with filenames

--- Chapter05/unittests/testExercise5_07.py
+++ Chapter05/unittests/testExercise5_07.py
@@ -1,7 +1,6 @@
 import unittest
-import numpy as np
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
+import numpy as np
 import os
 
 
@@ -9,28 +8,101 @@ class TestingExercise5_07(unittest.TestCase):
     def setUp(self) -> None:
         ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-        self.df = pd.read_csv(os.path.join(ROOT_DIR, '..', 'Datasets', 'breast-cancer-data.csv'))
+        self.df = pd.DataFrame()
+        self.df['Outlook'] = [
+            'sunny', 'sunny', 'overcast', 'rain', 'rain', 'rain',
+            'overcast', 'sunny', 'sunny', 'rain', 'sunny',
+            'overcast', 'overcast', 'rain'
+        ]
+        self.df['Temperature'] = [
+            'hot', 'hot', 'hot', 'mild', 'cool', 'cool', 'cool',
+            'mild', 'cool', 'mild', 'mild', 'mild', 'hot', 'mild',
+        ]
+        self.df['Humidity'] = [
+            'high', 'high', 'high', 'high', 'normal', 'normal', 'normal',
+            'high', 'normal', 'normal', 'normal', 'high', 'normal', 'high'
+        ]
+        self.df['Windy'] = [
+            'Weak', 'Strong', 'Weak', 'Weak', 'Weak', 'Strong', 'Strong', 'Weak', 'Weak', 'Weak',
+            'Strong', 'Strong', 'Weak', 'Strong'
+        ]
+        self.df['Decision'] = [
+            'N', 'N', 'P', 'P', 'P', 'N', 'P', 'N', 'P', 'P',
+            'P', 'P', 'P', 'N'
+        ]
 
-        np.random.seed(10)
-        samples = np.random.randint(0, len(self.df), 10)
-        self.df_test = self.df.iloc[samples]
-        self.df = self.df.drop(samples)
+        # Probability of P
+        p_p = len(self.df.loc[self.df.Decision == 'P']) / len(self.df)
+        # Probability of N
+        p_n = len(self.df.loc[self.df.Decision == 'N']) / len(self.df)
+        self.entropy_decision = -p_n * np.log2(p_n) - p_p * np.log2(p_p)
 
-        self.model = DecisionTreeClassifier()
-        self.model = self.model.fit(self.df[set(self.df.columns) - {'diagnosis'}], self.df.diagnosis)
+    def IG(self, data, column, ent_decision):
+        IG_decision = ent_decision
+        for name, temp in data.groupby(column):
+            p_p = len(temp.loc[temp.Decision == 'P']) / len(temp)
+            p_n = len(temp.loc[temp.Decision != 'P']) / len(temp)
+            entropy_decision = 0
+            if p_p != 0:
+                entropy_decision -= (p_p) * np.log2(p_p)
+            if p_n != 0:
+                entropy_decision -= (p_n) * np.log2(p_n)
+            IG_decision -= (len(temp) / len(self.df)) * entropy_decision
+        return IG_decision
+
+    @staticmethod
+    def f_entropy_decision(data):
+        p_p = len(data.loc[data.Decision == 'P']) / len(data)
+        p_n = len(data.loc[data.Decision == 'N']) / len(data)
+        return -p_n * np.log2(p_n) - p_p * np.log2(p_p)
 
     def test_dataframe_shape(self):
-        expected_shape = (559, 31)
+        expected_shape = (14, 5)
         actual_shape = self.df.shape
         self.assertEqual(actual_shape, expected_shape)
 
-    def test_train_set_accuracy(self):
-        train_accuracy = self.model.score(self.df[set(self.df.columns)-{'diagnosis'}], self.df.diagnosis)
-        self.assertEqual(train_accuracy, 1)
+    def test_entropy(self):
+        self.assertAlmostEqual(self.entropy_decision, 0.9402860, places=4)
 
-    def test_test_set_accuracy(self):
-        test_accuracy = self.model.score(self.df_test[set(self.df_test.columns)-{'diagnosis'}], self.df_test.diagnosis)
-        self.assertAlmostEqual(test_accuracy, 0.9, places=1)
+    def test_outlook_info_gain(self):
+        IG_decision_Outlook = self.entropy_decision  # H(S)
+        # Iterate through the values for outlook and compute the probabilities
+        # and entropy values
+        for name, Outlook in self.df.groupby('Outlook'):
+            num_p = len(Outlook.loc[Outlook.Decision == 'P'])
+            num_n = len(Outlook.loc[Outlook.Decision != 'P'])
+            num_Outlook = len(Outlook)
+            entropy_decision_outlook = 0
+            # Cannot compute log of 0 so add checks
+            if num_p != 0:
+                entropy_decision_outlook -= (num_p / num_Outlook) \
+                                            * np.log2(num_p / num_Outlook)
+            # Cannot compute log of 0 so add checks
+            if num_n != 0:
+                entropy_decision_outlook -= (num_n / num_Outlook) \
+                                            * np.log2(num_n / num_Outlook)
+            IG_decision_Outlook -= (num_Outlook / len(self.df)) * entropy_decision_outlook
+        self.assertAlmostEqual(IG_decision_Outlook, 0.24674982, places=4)
+
+    def test_individual_outlook_info_gains(self):
+        self.assertAlmostEqual(self.IG(self.df, 'Outlook', self.entropy_decision), 0.24674982, places=4)
+        self.assertAlmostEqual(self.IG(self.df, 'Temperature', self.entropy_decision), 0.02922256, places=4)
+        self.assertAlmostEqual(self.IG(self.df, 'Humidity', self.entropy_decision), 0.15183550, places=4)
+        self.assertAlmostEqual(self.IG(self.df, 'Windy', self.entropy_decision), 0.04812703, places=4)
+
+    def test_sunny_outlook_entropy(self):
+        df_next = self.df.loc[self.df.Outlook != 'overcast']
+        df_sunny = df_next.loc[df_next.Outlook == 'sunny']
+        entropy_decision = self.f_entropy_decision(df_sunny)
+        self.assertAlmostEqual(entropy_decision, 0.97095059, places=4)
+
+    def test_sunny_outlook_info_gain(self):
+        df_next = self.df.loc[self.df.Outlook != 'overcast']
+        df_sunny = df_next.loc[df_next.Outlook == 'sunny']
+        entropy_decision = self.f_entropy_decision(df_sunny)
+        self.assertAlmostEqual(self.IG(df_sunny, 'Temperature', entropy_decision), 0.82809345, places=4)
+        self.assertAlmostEqual(self.IG(df_sunny, 'Humidity', entropy_decision), 0.97095059, places=4)
+        self.assertAlmostEqual(self.IG(df_sunny, 'Windy', entropy_decision), 0.63131577, places=4)
 
 
 if __name__ == '__main__':
--- Chapter05/unittests/testExercise5_08.py
+++ Chapter05/unittests/testExercise5_08.py
@@ -1,9 +1,7 @@
 import unittest
-import struct
 import numpy as np
-import gzip
-from array import array
-from sklearn.neural_network import MLPClassifier
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
 import os
 
 
@@ -11,52 +9,28 @@ class TestingExercise5_08(unittest.TestCase):
     def setUp(self) -> None:
         ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-        with gzip.open(os.path.join(ROOT_DIR, '..', 'Datasets', 'train-images-idx3-ubyte.gz'), 'rb') as f:
-            magic, size, rows, cols = struct.unpack(">IIII", f.read(16))
-            img = np.array(array("B", f.read())).reshape((size, rows, cols))
+        self.df = pd.read_csv(os.path.join(ROOT_DIR, '..', 'Datasets', 'breast-cancer-data.csv'))
 
-        with gzip.open(os.path.join(ROOT_DIR, '..', 'Datasets', 'train-labels-idx1-ubyte.gz'), 'rb') as f:
-            magic, size = struct.unpack(">II", f.read(8))
-            labels = np.array(array("B", f.read()))
+        np.random.seed(10)
+        samples = np.random.randint(0, len(self.df), 10)
+        self.df_test = self.df.iloc[samples]
+        self.df = self.df.drop(samples)
 
-        with gzip.open(os.path.join(ROOT_DIR, '..', 'Datasets', 't10k-images-idx3-ubyte.gz'), 'rb') as f:
-            magic, size, self.rows, self.cols = struct.unpack(">IIII", f.read(16))
+        self.model = DecisionTreeClassifier()
+        self.model = self.model.fit(self.df[set(self.df.columns) - {'diagnosis'}], self.df.diagnosis)
 
-            img_test = np.array(array("B", f.read())).reshape((size, rows, cols))
+    def test_dataframe_shape(self):
+        expected_shape = (559, 31)
+        actual_shape = self.df.shape
+        self.assertEqual(actual_shape, expected_shape)
 
-        with gzip.open(os.path.join(ROOT_DIR, '..', 'Datasets', 't10k-labels-idx1-ubyte.gz'), 'rb') as f:
-            magic, size = struct.unpack(">II", f.read(8))
-            self.labels_test = np.array(array("B", f.read()))
-
-        np.random.seed(0)  # Give consistent random numbers
-        selection = np.random.choice(len(img), 5000)
-        self.selected_images = img[selection].reshape((-1, rows * cols))
-        self.selected_labels = labels[selection]
-
-        self.selected_images = self.selected_images / 255.0
-        self.img_test = img_test / 255.0
-
-        self.model = MLPClassifier(solver='sgd', hidden_layer_sizes=(100,), max_iter=1000, random_state=1,
-                              learning_rate_init=.01)
-        self.model.fit(X=self.selected_images, y=self.selected_labels)
-
-    def test_dimensions(self):
-        self.assertEqual(self.selected_images.shape, (5000, 784))
-        self.assertEqual(self.selected_labels.shape, (5000,))
-
-    def test_ann_train_accuracy(self):
-        train_accuracy = self.model.score(X=self.selected_images, y=self.selected_labels)
+    def test_train_set_accuracy(self):
+        train_accuracy = self.model.score(self.df[set(self.df.columns)-{'diagnosis'}], self.df.diagnosis)
         self.assertEqual(train_accuracy, 1)
 
-    def test_ann_test_accuracy(self):
-        test_accuracy = self.model.score(X=self.img_test.reshape((-1, self.rows * self.cols)), y=self.labels_test)
-        self.assertAlmostEqual(test_accuracy, 0.9384, places=4)
-
-    def test_predictions(self):
-        pred_label = self.model.predict(self.selected_images)[0]
-        pred_proba = self.model.predict_proba(self.selected_images)[0][4]
-        self.assertEqual(pred_label, 4)
-        self.assertAlmostEqual(pred_proba, 0.99999524, places=4)
+    def test_test_set_accuracy(self):
+        test_accuracy = self.model.score(self.df_test[set(self.df_test.columns)-{'diagnosis'}], self.df_test.diagnosis)
+        self.assertAlmostEqual(test_accuracy, 0.9, places=1)
 
 
 if __name__ == '__main__':
